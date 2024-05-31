9b6702bbdf2237cc2fc961f095c38c99c3e488dc
torosediles <torosediles10@gmail.com>
2024-05-31 20:35:45 +0200

    .eslintrc.js Updated

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,9 @@ module.exports = {
     "parserOptions": {
         "sourceType": "module"
     },
+    "globlas":{
+        "gtag":"readonly"
+    },
     "rules": {
         "no-cond-assign": [
             "error",
